103af4dc2a3433e29e85059a0f0375803f46ce08
Yihang Cai <Yihang.cai@mail.utoronto.ca>
2024-01-24 20:48:59 -0500

    2nd modified version

--- Scripts/00-simulate_data.R
+++ Scripts/00-simulate_data.R
@@ -8,13 +8,13 @@ duration <- 10
 # Simulate data for neighborhoods and crime types
 simulated_data <-
   tibble(
-    "Year" = start_year:(start_year + duration),
+    "Year" = start_year:(start_year + duration - 1),
     "Crime_types" = sample(
       x = c("Assault", "Burglary", "Robbery", "Theft"),
-      size = duration + 1,
+      size = duration,
       replace = TRUE
     ),
-    "Crime_rates" = runif(n = duration + 1, min = 0, max = 100) 
+    "Crime_rates" = runif(n = duration, min = 0, max = 100) 
   )
 
 
---
+++ Scripts/03-test_data.R
@@ -0,0 +1,7 @@
+wexford_table$Year |> min() == 2014
+wexford_table$Year |> max() == 2023
+ncol(wexford_table) == 7
+
+kensington_table$Year |> min() == 2014
+kensington_table$Year |> max() == 2023
+ncol(kensington_table) == 7
